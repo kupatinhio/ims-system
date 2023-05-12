a746cb720bbe40892fbbdfb841ec384f2be574e0
Kupatinhiio <zazaikesashvili@gmail.com>
2023-05-12 13:57:35 +0400

    create page module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RegisterModule } from './modules/register/register.module';
+import { PageModule } from './modules/page/page.module';
 
 @Module({
-  imports: [RegisterModule],
+  imports: [RegisterModule, PageModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/register/register.controller.ts
+++ src/modules/register/register.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { RegisterService } from './register.service';
 
-@Controller()
+@Controller('user')
 export class RegisterController {
   constructor(private readonly registerService: RegisterService) {}
+
+  //user
+  @Get()
+  index() {
+    return {
+      status:true,
+      message: 'User works!'
+    }
+  }
+
+  //user/register
+  @Get('register')
+  register() {
+    return {
+      status:true,
+      message: 'User register works!'
+    }    
+  }
+  //user/login
+  @Get('login')
+  login() {
+    return {
+      status:true,
+      message: 'User login works!'
+    }
+  }
 }
