9623e0f1b8b0b18b12e0faa34b97cac5ba3ce772
Kupatinhiio <zazaikesashvili@gmail.com>
2023-05-18 12:18:45 +0400

    Co-authored-by: Shota Salia <shotasalia@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RegisterModule } from './modules/register/register.module';
 import { PageModule } from './modules/page/page.module';
+import { UserModule } from './modules/user/user.module';
+
 
 @Module({
-  imports: [RegisterModule, PageModule],
+  imports: [RegisterModule, PageModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
