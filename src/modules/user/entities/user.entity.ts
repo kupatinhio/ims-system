dfaf2edf3861b24f3e2f89515403c9a6133b0e7e
Kupatinhiio <zazaikesashvili@gmail.com>
2023-05-18 13:21:41 +0400

    Co-authored-by: Shota Salia <shotasalia@users.noreply.github.com>

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { Column, Entity } from "typeorm";
+
+@Entity('users')
+export class User {
+
+    @Column("varchar")
+    public fName:string;
+
+    @Column("varchar")
+    public lName:string;
+    
+    @Column('varchar')
+    public email:string;
+    
+    @Column('number')
+    public tel:number;
+
+    @Column('string')
+    public password:string;
+
+    @Column('datetime')
+    public created_At: Date;
+    
+    @Column('datetime')
+    public updated_At: Date;
+}
